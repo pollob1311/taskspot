5dadac2083bccb74fac2f540319e2ef305477380
pollob1311 <pollob13112710@gmail.com>
2026-01-29 22:59:27 +0600

    update post

--- test-postback.js
+++ test-postback.js
@@ -6,14 +6,22 @@ async function testPostback() {
     const token = "YOUR_POSTBACK_TOKEN_HERE"; // If set in SystemSettings
 
     const params = new URLSearchParams({
+        // আমরা সব ধরণের নাম দিয়ে আইডি পাঠাচ্ছি
+        uid: userId,
         user_id: userId,
+        subId: userId,
+        click_id: userId,
+
         payout: "0.50",
-        network: "TimeWall",
+        network: "TimeWall", // নেটওয়ার্ক নাম TimeWall-ই থাকুক
+
+        // আপনার .env ফাইলে যদি কোনো POSTBACK_SECRET থাকে, সেটা এখানে বসান। 
+        // না জানলে আপাতত এই লাইনটি যেমন আছে তেমনই থাক।
         token: token,
+
         status: "success"
     });
-
-    const url = `https://taskspot.site/api/postback?${params.toString()}`;
+    const url = `http://localhost:3000/api/postback?${params.toString()}`;
 
     console.log(`Sending mock postback to: ${url}`);
 
